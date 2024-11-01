28feb9c2081803bae05fba3c2341c1baaeda1f42
Daniel Hansen <dchansen06@gmail.com>
2024-11-01 09:13:59 -0700

    Fixed errors

--- framegrab.sh
+++ framegrab.sh
@@ -4,7 +4,7 @@ start=$(date +%s%3N)
 
 HELP=("help" "h" "info" "?" "--help" "-h" "--info" "-?")
 
-if [[ "${HELP[@]}" =~ $1 ]]
+if [[ "${HELP[*]}" =~ $1 ]]
 then
 	echo "Set the video filename(s) to be the arguments, as many as needed"
 	echo "Set current directory to be the location where the frames should be"
@@ -12,14 +12,14 @@ then
 	echo "Specify by setting the first argument to the desired format (ie --tiff / --tif / -t)"
 	echo
 	echo "Supported options include"
-	echo "Display help		--help"
-	echo "Use jpeg		--jpeg, --jpg, -j"
-	echo "Use png		--png, -p"
-	echo "Use gif		--gif, -g"
-	echo "Use tiff		--tiff, -t"
-	echo "Use webp		--webp, --web, -w"
-	echo "Use ppm		--ppm, -m"
-	echo "Use bmp		--bmp, -b"
+	echo -e "Display help\t--help"
+	echo -e "Use jpeg\t--jpeg, --jpg, -j"
+	echo -e "Use png\t\t--png, -p"
+	echo -e "Use gif\t\t--gif, -g"
+	echo -e "Use tiff\t--tiff, -t"
+	echo -e "Use webp\t--webp, --web, -w"
+	echo -e "Use ppm\t\t--ppm, -m"
+	echo -e "Use bmp\t\t--bmp, -b"
 	exit 0
 fi
 
@@ -31,24 +31,24 @@ WEBP=("webp" "web" "w" "--webp" "--web" "-w")
 PPM=("ppm" "p" "--ppm" "-m")
 BMP=("bmp" "b" "--bmp" "-b")
 
-if [[ "${JPEG[@]}" =~ "$1" ]]; then shift; format="jpg"
-elif [[ "${PNG[@]}" =~ "$1" ]]; then shift; format="png"
-elif [[ "${GIF[@]}" =~ "$1" ]]; then shift; format="gif"
-elif [[ "${TIFF[@]}" =~ "$1" ]]; then shift; format="tiff"
-elif [[ "${WEBP[@]}" =~ "$1" ]]; then shift; format="webp"
-elif [[ "${PPM[@]}" =~ "$1" ]]; then shift; format="ppm"
-elif [[ "${BMP[@]}" =~ "$1" ]]; then shift; format="bmp"
+if [[ "${JPEG[*]}" =~ $1 ]]; then shift; format="jpg"
+elif [[ "${PNG[*]}" =~ $1 ]]; then shift; format="png"
+elif [[ "${GIF[*]}" =~ $1 ]]; then shift; format="gif"
+elif [[ "${TIFF[*]}" =~ $1 ]]; then shift; format="tiff"
+elif [[ "${WEBP[*]}" =~ $1 ]]; then shift; format="webp"
+elif [[ "${PPM[*]}" =~ $1 ]]; then shift; format="ppm"
+elif [[ "${BMP[*]}" =~ $1 ]]; then shift; format="bmp"
 else echo "No known format selected"; format="jpg"
 fi
 
 echo "Using $format"
 
 divide() {
-	echo "Grabbing frames from $(basename $1)"
-	ffmpeg -r 1 -i "$1" -r 1 "$(basename $1)%07d.$2"
+	echo "Grabbing frames from $(basename "$1")"
+	ffmpeg -r 1 -i "$1" -r 1 "$(basename "$1")%07d.$2"
 }
 
-for file in $@
+for file in "$@"
 do
 	if [[ $(file -i "$file") =~ "video" ]]
 	then
@@ -58,17 +58,17 @@ do
 
 		NO=("NO" "No" "no" "N" "n")
 		YES=("YES" "Yes" "yes" "Y" "y")
-		read response
+		read -r response
 
-		if [[ "${NO[@]}" =~ "$response" ]]
+		if [[ "${NO[*]}" =~ $response ]]
 		then
 			echo "Skipping $file"
-		elif [[ "${YES[@]}" =~ "$response" ]]
+		elif [[ "${YES[*]}" =~ $response ]]
 		then
 			echo "Reading $file regardless"
 			divide "$file" "$format"
 			echo "Press enter to continue"
-			read
+			read -r
 		else
 			echo "Exiting"
 			exit 1
